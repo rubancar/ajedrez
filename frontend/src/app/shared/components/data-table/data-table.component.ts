ecb330bb68d102869c4380d792019cf28ba6369d
Badwolf <rubancar@espol.edu.ec>
2022-01-04 16:39:04 +0100

    avance 1 datatable

---
+++ frontend/src/app/shared/components/data-table/data-table.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+
+import { JugadoresService } from 'src/app/services/jugadores.service';
+import { Jugador } from '../../entidades/jugador';
+
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  public displayedColumns: string[] = ['name', 'usuario', 'elo', 'responsable', 'es_moroso',  'fecha_registro'];
+  public columnsToDisplay: string[] = [...this.displayedColumns, 'actions'];
+
+  public columnsFilters = {};
+
+  public dataSource: MatTableDataSource<Jugador>;
+  private serviceSubscribe: Subscription;
+
+  constructor(private jugadorService: JugadoresService, public dialog: MatDialog) { 
+    this.dataSource = new MatTableDataSource<Jugador>();
+  }
+
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  /**
+   * initialize data-table by providing persons list to the dataSource.
+   */
+  ngOnInit(): void {
+    this.serviceSubscribe = this.jugadorService.getJugadores().subscribe(res => {
+      console.log(res);
+      this.dataSource.data = res;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.serviceSubscribe.unsubscribe();
+  }
+
+}
