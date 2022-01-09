8e6f558fe481f96c1ab7f0114c96630901de63ec
Badwolf <rubancar@espol.edu.ec>
2022-01-09 16:22:29 +0100

    Validación de formularios Jugador y Clubes

--- frontend/src/app/modules/clubes/dialog-club/dialog-club.component.ts
+++ frontend/src/app/modules/clubes/dialog-club/dialog-club.component.ts
@@ -14,6 +14,20 @@ export class DialogClubComponent implements OnInit {
   clubForm: FormGroup;
   club: Club;
 
+  erroresForm: any = {
+    'nombre':'',
+    'direccion':''
+  };
+
+  mensajesError: any = {
+    'nombre': {
+      'required': 'El nombre es obligatorio.'
+    },
+    'direccion': {
+      'required': 'La direccion es obligatoria.'
+    }
+  };
+
   constructor(private clubService:ClubService,
     public dialogRef: MatDialogRef<DialogClubComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Club) { 
@@ -22,15 +36,32 @@ export class DialogClubComponent implements OnInit {
         "id": new FormControl(''),
         "nombre": new FormControl('', Validators.required),
         "direccion": new FormControl('', Validators.required),
-        "usuario_entrenador": new FormControl('', Validators.required),
+        "usuario_entrenador": new FormControl(''),
         "federacion_id": new FormControl('')
-      })
+      });
 
+      this.clubForm.valueChanges.subscribe(datos => this.onCambioValor(datos));
       this.clubForm.setValue(data);
       this.club = new Club();
     }
 
   ngOnInit() {
+  }
+
+  onCambioValor(data?: any) {
+    if (!this.clubForm) { return; }
+    const form = this.clubForm;
+    for (const field in this.erroresForm) {
+      // Se borrarán los mensajes de error previos
+      this.erroresForm[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.mensajesError[field];
+        for (const key in control.errors) {
+          this.erroresForm[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
   onSubmit() {
