f597a7636e367e40634460e904db76bff6da2c71
Fernando Huertas Olivares <fernaku@gmail.com>
2022-01-13 19:18:19 +0100

    Mejora visual de la app

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsuariosService } from '../../../services/usuarios.service';
+import { Usuario } from '../../entidades/usuario';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +10,18 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  private loggedUser;
+  private user : Usuario;
   @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
+  constructor(private usuariosService: UsuariosService,private router: Router) { }
 
-  constructor(private router: Router) { }
-
-  ngOnInit() { }
+  ngOnInit() {
+    this.loggedUser = localStorage.getItem("currentUser");
+    if (this.loggedUser != null && this.loggedUser != ""){
+      this.usuariosService.getUsuario(this.loggedUser).subscribe(user => {this.user = user});
+    }
+      console.log("inciando menu principal")
+  }
 
   toggleSideBar() {
     this.toggleSideBarForMe.emit();
--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuariosService } from '../../../services/usuarios.service';
-import { Usuario } from '../../entidades/usuario';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,16 +6,7 @@ import { Usuario } from '../../entidades/usuario';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  private loggedUser;
-  private user : Usuario;
-  constructor(private usuariosService: UsuariosService,) { }
 
-  ngOnInit() {
-    this.loggedUser = localStorage.getItem("currentUser");
-    if (this.loggedUser != null && this.loggedUser != ""){
-      this.usuariosService.getUsuario(this.loggedUser).subscribe(user => {this.user = user});
-    }
-      console.log("inciando menu principal")
-  }
+  constructor() { }
 
 }
