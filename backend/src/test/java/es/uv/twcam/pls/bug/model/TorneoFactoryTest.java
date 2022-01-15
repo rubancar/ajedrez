897d393341dd11d4a83786dc14e27b960a6ce990
Alexis Janero Moliner <ajanerom@gmail.com>
2022-01-15 19:10:37 +0100

    adding pruebas TorneoFactory

--- backend/src/test/java/es/uv/twcam/pls/bug/model/TorneoFactoryTest.java
+++ backend/src/test/java/es/uv/twcam/pls/bug/model/TorneoFactoryTest.java
@@ -1,10 +1,11 @@
 package es.uv.twcam.pls.bug.model;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
+//import org.junit.jupiter.api.TestMethodOrder;
+//import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class TorneoFactoryTest {
   static String id = null;
   static String name;
   static String sede;
-  static ArrayList<String> jugadores;
+  static ArrayList<String> jugadores = new ArrayList();
   static ArrayList<Partida> partidas;
   public static Torneo torneo1;
 
@@ -22,27 +23,32 @@ public class TorneoFactoryTest {
   @BeforeAll
   public static void setUpClass() {
 
-    Jugador jugador1 = new Jugador();
-    Jugador jugador2 = new Jugador();
-    Jugador jugador3 = new Jugador();
-    jugador1.setId("1");
-    jugador1.setName("Pepe");
-    jugador2.setId("2");
-    jugador2.setName("Juan");
-    jugador3.setId("3");
-    jugador3.setName("Pedro");
-    System.out.println("test id jugador: " + jugador2.getId());
-    jugadores.add(jugador1.getId());
-    jugadores.add(jugador2.getId());
-    jugadores.add(jugador3.getId()); 
     torneo1 = new Torneo(null, name, sede, jugadores, partidas);
-    System.out.println(torneo1);
 
+  }
+  
+  @BeforeEach
+  public void setUp() {
+	    Jugador jugador1 = new Jugador();
+	    Jugador jugador2 = new Jugador();
+	    Jugador jugador3 = new Jugador();
+	    jugador1.setId("1");
+	    jugador1.setName("Pepe");
+	    jugador2.setId("2");
+	    jugador2.setName("Juan");
+	    jugador3.setId("3");
+	    jugador3.setName("Pedro");
+	    System.out.println("test id jugador: " + jugador2.getId());
+	    jugadores.add(jugador1.getId());
+	    jugadores.add(jugador2.getId());
+	    jugadores.add(jugador3.getId());      
   }
 
   @Test
   void testCreate() throws Exception {
-		Torneo torneo2 = TorneoFactory.getInstance().create(torneo1);
+	    System.out.println("test id jugador: " + jugadores);
+
+	    Torneo torneo2 = TorneoFactory.getInstance().create(torneo1);
 		
 		TorneoFactoryTest.id = torneo1.getId();
 		
